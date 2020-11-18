70aa48244362b34d445cfc5c157edffcffe383e4
hyunmoyan <pearlisrich@naver.com>
2020-11-18 12:26:32 +0900

    Update load_list.py

--- load_list.py
+++ load_list.py
@@ -1,7 +1,25 @@
 import pickle
 
-with open("./data/book_list1.txt", "rb") as fp:   # Unpickling
-    load_list = pickle.load(fp)
-    print(load_list)
-    print(type(load_list))
+with open("./data/book_list_author.txt", "rb") as fp:   # Unpickling
+    author_list = pickle.load(fp)
+    print(author_list)
+    print(type(author_list))
+
+
+with open("./data/book_list_img.txt", "rb") as fp:   # Unpickling
+    img_list = pickle.load(fp)
+    print(img_list)
+    print(type(img_list))
+
+
+with open("./data/book_list_title.txt", "rb") as fp:   # Unpickling
+    title_list = pickle.load(fp)
+    print(title_list)
+    print(type(title_list))
+
+
+with open("./data/book_list_url.txt", "rb") as fp:   # Unpickling
+    url_list = pickle.load(fp)
+    print(url_list)
+    print(type(url_list))
 
